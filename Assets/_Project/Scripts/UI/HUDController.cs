0fb9480674299e59a71b2a71ed2b3c7c683d0105
unknown <oleh.valdemar@gmail.com>
2025-11-02 13:24:56 +0200

    feat(UI): add default button and restart

--- Assets/_Project/Scripts/UI/HUDController.cs
+++ Assets/_Project/Scripts/UI/HUDController.cs
@@ -1,4 +1,5 @@
 using Project.Services.SaveSystem;
+using Project.Services.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 using VContainer;
@@ -8,24 +9,32 @@ namespace Project.UI
     public class HUDController : MonoBehaviour
     {
         [SerializeField] private Text coinText;
+        [SerializeField] private Button restartButton;
 
         private ISaveService _saveService;
+        private ISceneLoader _sceneLoader;
 
         [Inject]
-        public void Construct(ISaveService saveService)
+        public void Construct(ISaveService saveService, ISceneLoader sceneLoader)
         {
             _saveService = saveService;
+            _sceneLoader = sceneLoader;
         }
 
         private void Start()
         {
             UpdateCoinsUI();
+            restartButton.onClick.AddListener(OnRestartButtonClicked);
         }
 
         public void UpdateCoinsUI()
         {
             int coins = _saveService.Load("coins", 0);
             coinText.text = $"Coins: {coins}";
+        }
+        private void OnRestartButtonClicked()
+        {
+            _sceneLoader.ReloadCurrentSceneAsync();
         }
     }
 }

066894e73ae2d8895877c6616bb1904155dca328
unknown <oleh.valdemar@gmail.com>
2025-11-02 02:56:58 +0200

    fix(player): change player speed

--- Assets/_Project/Scripts/Player/PlayerView.cs
+++ Assets/_Project/Scripts/Player/PlayerView.cs
@@ -7,8 +7,8 @@ namespace Project.Player
     {
         private Rigidbody _rigidbody;
         public Rigidbody Rigidbody => _rigidbody ??= GetComponent<Rigidbody>();
-        [field: SerializeField, Range(0f, 20f)] public float MoveSpeed { get; private set; } = 5f;
-        [field: SerializeField, Range(0f, 50f)] public float MaxVelocity { get; private set; } = 10f;
+        [field: SerializeField, Range(20f, 400f)] public float MoveSpeed { get; private set; } = 5f;
+        [field: SerializeField, Range(20f, 800)] public float MaxVelocity { get; private set; } = 10f;
         // Event to notify Presenter
         public event Action<Collider> OnPlayerTriggerEnter;
 
